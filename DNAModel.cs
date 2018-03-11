4de4cec1fad142a9d0d324a71bade5ca692e91ba
Mikira Sora <mikirasora0409@126.com>
2018-03-11 13:30:27 +0800

    add Readme

--- DNAModel.cs
+++ DNAModel.cs
@@ -26,19 +26,48 @@ namespace StorybrewScripts
         [Configurable]
         public int Count = 50;
 
+        [Configurable]
+        public int StartX=-107;
+
+        [Configurable]
+        public int Width=854;
+
+        [Configurable]
+        public int BaseY=240;
+
+        [Configurable]
+        public int Height=200;
+
+        [Configurable]
+        public int MoveXInv=100;
+
+        [Configurable]
+        public int MoveYInv=1000;
+
+        [Configurable]
+        public int MoveXUnitInv=200;
+
         private OsbSprite GenerateSprite(string path)
         {
             var layer = GetLayer("LineEffect");
-            var sprite = layer.CreateSprite(@"SB\spetrum\1.png", OsbOrigin.BottomCentre);
-            sprite.Fade(OsbEasing.None, StartTime, EndTime, 1, 1);
+            var sprite = layer.CreateSprite(path, OsbOrigin.Centre);
 
             return sprite;
         }
         public override void Generate()
         {
+            float center_base_scale=Height/126.0f;
+
             for (int i = 0; i < Count; i++)
             {
-                
+                float start_x_offset=(i/(float)Count)*Width;
+                //height=126
+
+                float start_y_offset=(((int)start_x_offset)%MoveXUnitInv/MoveXUnitInv-0.5f)*Height;
+
+                #region //todo sprite center
+                var sprite_center=GenerateSprite(@"SB\spetrum\1.png");
+                #endregion
             }
         }
     }
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Osu!Storyborad Project : 霜月はるか - Libume
+---
+![](https://puu.sh/zF5Qs/6b6d7c4cf4.bmp)
